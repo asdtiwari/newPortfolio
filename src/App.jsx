23bb3beaabc50844e48eb8dec2db267cd33425a2
Akhilesh Tiwari <babitiwari9@hotmail.com>
2025-01-18 11:07:13 +0530

    Initialised the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="w-full h-full">
+        <h1 className="text-3xl font-bold underline w-fit mx-auto">Hello Akhilesh</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
